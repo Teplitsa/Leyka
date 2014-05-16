ceef0aa1864292bcb896cb2203c20226fa761735
DeeKey <privateinf@gmail.com>
2014-05-16 20:11:14 +0400

    Update leyka.php

--- leyka.php
+++ leyka.php
@@ -3,7 +3,7 @@
  * Plugin Name: Leyka
  * Plugin URI:  http://leyka.te-st.ru/
  * Description: The donations management system for your WP site.
- * Version:     2.1.1
+ * Version:     2.1.2
  * Author:      Lev Zvyagincev aka Ahaenor
  * Author URI:  ahaenor@gmail.com
  * Text Domain: leyka
