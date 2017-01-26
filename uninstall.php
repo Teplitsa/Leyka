966daa6b108d7d8bc482548b9a2a6740ca68d471
ahaenor <ahaenor@gmail.com>
2017-01-26 19:00:34 +0300

    * DB cleanup function is ready.

--- uninstall.php
+++ uninstall.php
@@ -16,6 +16,16 @@ if(get_option('leyka_delete_plugin_data')) { // Completely remove all campaigns 
         wp_delete_post($id, true); // All revisions, post metas and comments will also be deleted
     }
 
+    $success_page = get_post(get_option('leyka_success_page'));
+    if(stristr($success_page->post_name, 'thank-you-for-your-donation') !== false) {
+        wp_delete_post($success_page->ID, true);
+    }
+
+    $failure_page = get_post(get_option('leyka_failure_page'));
+    if(stristr($failure_page->post_name, 'sorry-donation-failure') !== false) {
+        wp_delete_post($failure_page->ID, true);
+    }
+
 }
 
 if(get_option('leyka_delete_plugin_options')) {
