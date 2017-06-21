6b0d71584a21f1faff2bdb71b385f7067839719d
ahaenor <ahaenor@gmail.com>
2017-06-21 13:23:35 +0300

    * Yandex.Kassa recurring pre-release testing.

--- gateways/yandex/leyka-class-yandex-gateway.php
+++ gateways/yandex/leyka-class-yandex-gateway.php
@@ -344,6 +344,7 @@ shopId="'.leyka_options()->opt('yandex_shop_id').'"/>');
 
     public function cancel_recurring_subscription(Leyka_Donation $donation) {
 
+        echo '<pre>' . print_r($donation->type.' - '.(int)$donation->recurring_is_active, 1) . '</pre>';
         if($donation->type == 'rebill' && $donation->recurring_is_active) {
             echo '<pre>' . print_r($donation, 1) . '</pre>';
             $donation->recurring_subscription_cancelled = true;
