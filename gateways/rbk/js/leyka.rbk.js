8aed919a56859044300569489dbc2a422988f109
petrozavodsky <petrozavodsky@gmail.com>
2018-08-30 22:35:00 +0300

    Refactoring js file

--- gateways/rbk/js/leyka.rbk.js
+++ gateways/rbk/js/leyka.rbk.js
@@ -1,14 +1,14 @@
-var $             = jQuery.noConflict();
-leyka_rbk_gateway = {
-	run : function () {
-		$( window ).on( 'leyka_submission_form_data', function ( type, response ) {
-			$( '.leyka-pf--active' ).removeClass( 'leyka-pf--active' );
-			eval( response.script );
-		} );
-	}
+var $ = jQuery.noConflict();
+var leyka_rbk_gateway = {
+    run: function () {
+        $(window).on('leyka_submission_form_data', function (type, response) {
+            $('.leyka-pf--active').removeClass('leyka-pf--active');
+            eval(response.script);
+        });
+    }
 };
 
 
-$( document ).ready( function () {
-	leyka_rbk_gateway.run();
-} );
+$(document).ready(function () {
+    leyka_rbk_gateway.run();
+});
