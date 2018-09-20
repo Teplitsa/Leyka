0fb3190842ecc89a01a792a3408142fdf6e7b739
DenisCherniatev <denis.cherniatev@gmail.com>
2018-09-20 16:43:49 +0200

    * cp wizard final screen fixed

--- inc/settings-fields-templates/leyka-cp_final.php
+++ inc/settings-fields-templates/leyka-cp_final.php
@@ -4,17 +4,44 @@
 
 /** @var $this Leyka_Custom_Setting_Block A block for which the template is used. */
 
-$campaign_id = get_transient('leyka_init_campaign_id');
-$campaign_url_encoded = urlencode(get_the_permalink($campaign_id));
+$campaigns = array(); get_posts(array(
+            'post_type' => Leyka_Campaign_Management::$post_type,
+            'post_status' => array('publish', 'pending', 'draft'),
+            'posts_per_page'   => 1,
+            'fields' => 'ids',
+        ));
+
+$campaign_id = count($campaigns) ? $campaigns[0] : null;
+$campaign_url = $campaign_id ? get_the_permalink($campaign_id) : null;
+$campaign_url_encoded = $campaign_url ? urlencode($campaign_url) : null;
 
 ?>
 
 <div class="<?php echo $this->field_type;?> cp-final-share-campaign">
 
+<?php if($campaign_id):?>
+    <!--
+    <p>Поделитесь вашей последней кампанией с друзьями и попросите их отправить пожертвование. Так вы сможете протестировать новый способ оплаты.</p>
+    -->
+    <p>Перейдите на страницу вашей кампании, чтобы протестировать ее.</p>
+<?php else:?>
+    <p>Создайте кампанию, чтобы начать сбор пожертвований.</p>
+<?php endif?>
+
+<div class="final-button">
+    <?php if($campaign_id):?>
+        <a class="step-next button button-primary" href="<?php echo $campaign_url?>" target="_blank">Протестировать кампанию</a>
+    <?php else:?>
+        <a class="step-next button button-primary" href="<?php echo admin_url("/admin.php?page=leyka_settings_new&screen=wizard-init")?>">Создать кампанию</a>
+    <?php endif?>
+</div>
+
+<!--
 <div class="share-campaign">
     <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $campaign_url_encoded?>" target="_blank"><img src="<?php echo LEYKA_PLUGIN_BASE_URL?>img/icon-share-fb.svg"/></a>
     <a href="https://vk.com/share.php?url=<?php echo $campaign_url_encoded?>" target="_blank"><img src="<?php echo LEYKA_PLUGIN_BASE_URL?>img/icon-share-vk.svg"/></a>
     <a href="https://connect.ok.ru/offer?url=<?php echo $campaign_url_encoded?>" target="_blank"><img src="<?php echo LEYKA_PLUGIN_BASE_URL?>img/icon-share-od.svg"/></a>
 </div>
+-->
 
 </div>
