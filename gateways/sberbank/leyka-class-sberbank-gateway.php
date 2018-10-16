f45c617c5f8b722aaa0df304416ca05c903f4b9d
Georgy Vlasov <wlasowegor@gmail.com>
2018-10-16 07:47:39 +0500

    Debug

--- gateways/sberbank/leyka-class-sberbank-gateway.php
+++ gateways/sberbank/leyka-class-sberbank-gateway.php
@@ -79,15 +79,7 @@ class Leyka_Sberbank_Gateway extends Leyka_Gateway
     /** Quittance don't use any specific redirects, so this method is empty. */
     public function submission_redirect_url($current_url, $pm_id)
     {
-        throw new ErrorException(
-            var_export(
-                [
-                    $current_url,
-                    $pm_id
-                ],
-                true
-            )
-        );
+        return home_url('/чё');
     }
 
     /** Quittance don't have some form data to send to the gateway site */
