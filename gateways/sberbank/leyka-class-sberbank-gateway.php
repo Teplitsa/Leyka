a60be0cda8122d343f30ffa25d74a50a469730e6
Georgy Vlasov <wlasowegor@gmail.com>
2018-10-20 18:02:49 +0500

    Implement payments

--- gateways/sberbank/leyka-class-sberbank-gateway.php
+++ gateways/sberbank/leyka-class-sberbank-gateway.php
@@ -1,4 +1,7 @@
-<?php use Voronkovich\SberbankAcquiring\Client;
+<?php
+require_once __DIR__ . '/../../www/vendor/autoload.php';
+
+use Voronkovich\SberbankAcquiring\Client;
 use Voronkovich\SberbankAcquiring\Currency;
 
 if (!defined('WPINC')) die;
@@ -11,17 +14,20 @@ class Leyka_Sberbank_Gateway extends Leyka_Gateway
 
     protected static $_instance; // Gateway is always a singleton
 
+    /** @var string */
+    private $redirectUrl;
+
     protected function _set_options_defaults()
     {
         $this->_options = array(
-            'login' => array(
+            'sberbank-acquiring-login' => array(
                 'type' => 'text',
                 'default' => '',
                 'title' => __('Sberbank payment gateway login', 'leyka'),
                 'required' => 1,
                 'placeholder' => __('Sberbank payment gateway login', 'leyka'),
             ),
-            'password' => array(
+            'sberbank-acquiring-password' => array(
                 'type' => 'text',
                 'default' => '',
                 'title' => __('Sberbank payment gateway password', 'leyka'),
@@ -47,61 +53,71 @@ class Leyka_Sberbank_Gateway extends Leyka_Gateway
 
     public function process_form($gateway_id, $pm_id, $donation_id, $form_data)
     {
-        throw new ErrorException(
-            var_export(
-                $form_data,
-                true
-            )
-        );
-        $username = null;
-        $password = null;
-        $orderAmount = null;
-        $returnUrl = 'https://zadolba.li';
+        $username = leyka_options()->opt('sberbank-acquiring-login');
+        $password = leyka_options()->opt('sberbank-acquiring-password');
+        $donation = new Leyka_Donation($donation_id);
+        $orderAmount = $form_data['leyka_donation_amount'];
         $client = new Client(
             [
                 'userName' => $username,
-                'password' => $password
+                'password' => $password,
+                'apiUri' => Client::API_URI_TEST,
             ]
         );
         $result = $client->registerOrder(
             $donation_id,
-            $orderAmount,
-            $returnUrl,
+            $orderAmount * 100,
+            leyka_get_success_page_url(),
             [
                 'currency' => Currency::RUB,
-                'failUrl' => 'http://fail.com'
+                'failUrl' => leyka_get_failure_page_url(),
             ]
         );
-        $paymentFormUrl = $result['formUrl'];
-        header('Location: ' . $paymentFormUrl);
+        if (!isset($result['errorCode'])) {
+            $donation->add_gateway_response(serialize($result));
+            $this->redirectUrl = $result['formUrl'];
+            header('Location: ' . $this->redirectUrl);
+            exit();
+        }
     }
 
-    /** Quittance don't use any specific redirects, so this method is empty. */
     public function submission_redirect_url($current_url, $pm_id)
     {
-        return home_url('/чё');
+        return $this->redirectUrl;
     }
 
-    /** Quittance don't have some form data to send to the gateway site */
     public function submission_form_data($form_data_vars, $pm_id, $donation_id)
     {
-        return $form_data_vars;
+//        $donation = new Leyka_Donation($donation_id);
+//        $post = get_post($donation_id);
+//        return [
+//            'userName' => leyka_options()->opt('sberbank-acquiring-login'),
+//            'password' => leyka_options()->opt('sberbank-acquiring-password'),
+//            'currency' => 643, // rur
+//            'orderNumber' => $donation_id,
+//            'amount' => $donation->amount * 100,
+//            'description' => $post->post_title,
+//            'returnUrl' => leyka_get_success_page_url(),
+//            'failUrl' => leyka_get_failure_page_url(),
+//        ];
     }
 
-    /** Quittance don't have any of gateway service calls */
     public function _handle_service_calls($call_type = '')
     {
     }
 
     public function get_gateway_response_formatted(Leyka_Donation $donation)
     {
-        return array();
+        $response_vars = unserialize($donation->gateway_response);
+        return array(
+            __('Order Id:', 'leyka') => $response_vars['orderId'],
+        );
     }
 
-    /** Quittance don't use any specific fields, so this method is empty. */
     public function log_gateway_fields($donation_id)
     {
     }
+
 }
 
 class Leyka_Sberbank_Acquiring extends Leyka_Payment_Method
@@ -115,17 +131,17 @@ class Leyka_Sberbank_Acquiring extends Leyka_Payment_Method
     {
         $this->_id = self::PAYMENT_METHOD_ID;
         $this->_gateway_id = 'sberbank';
-        $this->_label_backend = __('Bank order sberbank', 'leyka');
-        $this->_label = __('Bank order sberbank', 'leyka');
+        $this->_label_backend = __('Sberbank acquiring', 'leyka');
+        $this->_label = __('Sberbank acquiring', 'leyka');
         $this->_icons = apply_filters(
             'leyka_icons_' . $this->_gateway_id . '_' . $this->_id, array(
                 LEYKA_PLUGIN_BASE_URL . 'gateways/sberbank/icons/sber_s.png',
             )
         );
-        $this->_submit_label = __('Get bank order sberbank', 'leyka');
+        $this->_submit_label = __('Sberbank acquiring', 'leyka');
         $this->_supported_currencies = array('rur');
         $this->_default_currency = 'rur';
-        $this->_ajax_without_form_submission = true;
+        $this->_ajax_without_form_submission = false;
     }
 
     protected function _set_options_defaults()
