a7eb6a293b5ac42f494c7de3be7607bf7f488e8c
Ahaenor <ahaenor@gmail.com>
2018-11-06 17:55:16 +0300

    * Fixes in ajax donation forms handling.

--- inc/leyka-ajax.php
+++ inc/leyka-ajax.php
@@ -95,7 +95,7 @@ function leyka_get_gateway_redirect_data() {
         }
 
         $payment_vars = array(
-            'status' => $donation_id && !is_wp_error($donation_id) ? 0 : 1,
+            'status' => $donation_id && !is_wp_error($donation_id) && empty(leyka()->payment_form_has_errors()) ? 0 : 1,
             'payment_url' => apply_filters('leyka_submission_redirect_url-'.$pm['gateway_id'], '', $pm['payment_method_id']),
             'submission_redirect_type' => apply_filters(
                 'leyka_submission_redirect_type-'.$pm['gateway_id'],
@@ -103,10 +103,20 @@ function leyka_get_gateway_redirect_data() {
             ),
         );
 
-        if($payment_vars['status'] == 0) {
+        if( !$payment_vars['status'] ) {
             $payment_vars['donation_id'] = $donation_id;
-        } else {
+        } else if(is_wp_error($donation_id)) {
+
             $payment_vars['errors'] = $donation_id;
+            $payment_vars['message'] = $donation_id->get_error_message();
+
+        } else if(leyka()->payment_form_has_errors()) {
+
+            $error = reset(leyka()->get_payment_form_errors());
+
+            $payment_vars['errors'] = $error;
+            $payment_vars['message'] = $error->get_error_message();
+
         }
 
         $payment_vars = array_merge(
