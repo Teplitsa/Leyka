7027b1d88750dff0688d53ef345f2e8f05d333e3
Ahaenor <ahaenor@gmail.com>
2018-11-22 16:12:38 +0300

    * Small codestyle fixes.

--- leyka.php
+++ leyka.php
@@ -113,9 +113,10 @@ if( !$gateways_dir ) {
 
         $file_addr = LEYKA_PLUGIN_DIR."gateways/$gateway_id/leyka-class-$gateway_id-gateway.php";
 
-        if($gateway_id != '.' && $gateway_id != '..' && file_exists($file_addr)) {
+        if($gateway_id !== '.' && $gateway_id !== '..' && file_exists($file_addr)) {
             require_once($file_addr);
         }
+
     }
 
     $gateways_dir->close();
