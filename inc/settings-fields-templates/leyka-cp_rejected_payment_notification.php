ea773a45747dde79065ec249b8184adf72a41d42
Ahaenor <ahaenor@gmail.com>
2018-12-10 21:57:52 +0300

    * CP Wizard - content typo fix.

--- inc/settings-fields-templates/leyka-cp_rejected_payment_notification.php
+++ inc/settings-fields-templates/leyka-cp_rejected_payment_notification.php
@@ -6,18 +6,18 @@
 
 <div class="<?php echo $this->field_type;?> custom-block-captioned-screens">
 
-<span class="info2copy leyka-wizard-copy2clipboard"><?php echo site_url('/leyka/service/cp/complete/');?></span>
+    <span class="info2copy leyka-wizard-copy2clipboard"><?php echo site_url('/leyka/service/cp/fail/');?></span>
 
-<div class="captioned-screen">
+    <div class="captioned-screen">
 
-    <p><?php esc_html_e('Turn on the "Fail" field flag and insert the URL to the field, as screenshot shows:', 'leyka');?></p>
+        <p><?php esc_html_e('Turn on the "Fail" field flag and insert the URL to the field, as screenshot shows:', 'leyka');?></p>
 
-    <div class="screen-wrapper">
-        <img src="<?php echo LEYKA_PLUGIN_BASE_URL?>img/cp/cp_rejected_payment_notification.png" class="leyka-instructions-screen" alt="">
-        <img src="<?php echo LEYKA_PLUGIN_BASE_URL?>img/icon-zoom-screen.svg" class="zoom-screen" alt="">
+        <div class="screen-wrapper">
+            <img src="<?php echo LEYKA_PLUGIN_BASE_URL?>img/cp/cp_rejected_payment_notification.png" class="leyka-instructions-screen" alt="">
+            <img src="<?php echo LEYKA_PLUGIN_BASE_URL?>img/icon-zoom-screen.svg" class="zoom-screen" alt="">
+        </div>
+        <img src="<?php echo LEYKA_PLUGIN_BASE_URL?>img/cp/cp_rejected_payment_notification.png" class="leyka-instructions-screen-full" alt="">
+
     </div>
-    <img src="<?php echo LEYKA_PLUGIN_BASE_URL?>img/cp/cp_rejected_payment_notification.png" class="leyka-instructions-screen-full" alt="">
-
-</div>
 
 </div>
