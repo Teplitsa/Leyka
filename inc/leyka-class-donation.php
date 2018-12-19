89664411b63a4188b0e39e0dc96334632299b051
Ahaenor <ahaenor@gmail.com>
2018-12-19 17:34:11 +0300

    * Now admin pages show full date & time for donations.

--- inc/leyka-class-donation.php
+++ inc/leyka-class-donation.php
@@ -1741,10 +1741,11 @@ class Leyka_Donation {
             case 'date_label':
                 $date_format = get_option('date_format');
                 $time_format = get_option('time_format');
-                $donation_timestamp = strtotime($this->_post_object->post_date);
+                $donation_timestamp = $this->date_timestamp;
+
                 return apply_filters(
                     'leyka_admin_donation_date',
-                    date($date_format, $donation_timestamp),
+                    date("$date_format, $time_format", $donation_timestamp),
                     $donation_timestamp, $date_format, $time_format
                 );
             case 'date_timestamp': return strtotime($this->_post_object->post_date);
