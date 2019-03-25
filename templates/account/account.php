82055dd65f9c0dcf905346c71f1b12e1248b31a2
DenisCherniatev <denis.cherniatev@gmail.com>
2019-03-25 11:42:06 +0200

    * donor account markup updated

--- templates/account/account.php
+++ templates/account/account.php
@@ -28,38 +28,64 @@ include(LEYKA_PLUGIN_DIR . 'templates/account/account-header.php'); ?>
 							
 							<p>Мы благодарны вам за оказываемую поддержку!</p>
 							
-							<div class="leyka-star-history">
-								<div class="item break">
-									<h2>Отключение</h2>
-									<span class="date">12.01.2019</span>
-									<p>«Помогите изданию оставаться независимым источником информации»</p>
+							<div class="list subscribed-campaigns-list">
+								<h3 class="list-title">Кампании с ежемесячными пожертвованиями</h3>
+								<div class="items">
+									<div class="item">
+										<span class="campaign-title">Помогите изданию оставаться независимым источником информации</span>
+										<span class="amount">300 Р./мес.</span>
+									</div>
+									<div class="item">
+										<span class="campaign-title">На погашение штрафа от Роскомнадзора</span>
+										<span class="amount">1800 Р./мес.</span>
+									</div>
+									<div class="item">
+										<span class="campaign-title">Поможем Григорию переехать </span>
+										<span class="amount">210 Р./мес.</span>
+									</div>
 								</div>
-								<div class="item no-pay">
-									<h2>300 Р.</h2>
-									<span class="date">12.01.2019</span>
-									<p>«Помогите изданию оставаться независимым источником информации»</p>
+							</div>
+							
+							<div class="list leyka-star-history">
+								<h3 class="list-title">История пожертвований</h3>
+								<div class="items">
+									<div class="item break">
+										<h4 class="item-title">Отключение</h4>
+										<span class="date">12.01.2019</span>
+										<p>«Помогите изданию оставаться независимым источником информации»</p>
+									</div>
+									<div class="item no-pay">
+										<h4 class="item-title">300 Р.</h4>
+										<span class="date">12.01.2019</span>
+										<p>«Помогите изданию оставаться независимым источником информации»</p>
+									</div>
+									<div class="item error">
+										<h4 class="item-title">300 Р.</h4>
+										<span class="date">12.01.2019</span>
+										<p>«Помогите изданию оставаться независимым источником информации»</p>
+									</div>
+									<div class="item pay">
+										<h4 class="item-title">300 Р.</h4>
+										<span class="date">12.01.2019</span>
+										<p>«Помогите изданию оставаться независимым источником информации»</p>
+									</div>
+									<div class="item break">
+										<h4 class="item-title">Отключение</h4>
+										<span class="date">12.01.2019</span>
+										<p>«Помогите изданию оставаться независимым источником информации»</p>
+									</div>
+									<div class="item refund">
+										<h4 class="item-title">Отключение</h4>
+										<span class="date">12.01.2019</span>
+										<p>«Помогите изданию оставаться независимым источником информации»</p>
+									</div>
 								</div>
-								<div class="item error">
-									<h2>300 Р.</h2>
-									<span class="date">12.01.2019</span>
-									<p>«Помогите изданию оставаться независимым источником информации»</p>
-								</div>
-								<div class="item pay">
-									<h2>300 Р.</h2>
-									<span class="date">12.01.2019</span>
-									<p>«Помогите изданию оставаться независимым источником информации»</p>
-								</div>
-								<div class="item break">
-									<h2>Отключение</h2>
-									<span class="date">12.01.2019</span>
-									<p>«Помогите изданию оставаться независимым источником информации»</p>
+								
+								<div class="leyka-star-submit">
+									<a href="#" class="leyka-star-btn">Загрузить еще</a>
 								</div>
 							</div>
 						
-							<div class="leyka-star-submit">
-								<a href="#" class="leyka-star-btn">Загрузить еще</a>
-							</div>
-							
 							<p class="leyka-we-need-you">Вы всегда можете <a href="?leyka-screen=cancel-subscription">отключить ваше ежемесячное пожертвование.</a><br />Но нам будет без вас трудно.</p>
 							
 						</form>
