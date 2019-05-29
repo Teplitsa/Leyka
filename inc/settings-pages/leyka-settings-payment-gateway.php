cdaccc18523f551fea21802cf8065486fedd59b7
Ahaenor <ahaenor@gmail.com>
2019-05-29 16:16:04 +0300

    * CSS bugfixes in Wizards.

--- inc/settings-pages/leyka-settings-payment-gateway.php
+++ inc/settings-pages/leyka-settings-payment-gateway.php
@@ -79,7 +79,7 @@ if( !$gateway ) {?>
 
         if($gateway->has_wizard) {?>
             <a class="gateway-header-element gateway-wizard-link" href="<?php echo $gateway->wizard_url;?>" title="<?php esc_attr_e('Open the gateway setup wizard', 'leyka');?>">
-                <img src="<?php echo LEYKA_PLUGIN_BASE_URL;?>img/icon-wizard.svg" alt="">&nbsp;<?php esc_html_e('Step-by-step setup', 'leyka');?>
+                <img src="<?php echo LEYKA_PLUGIN_BASE_URL;?>img/icon-wizard.svg" alt=""><?php esc_html_e('Step-by-step setup', 'leyka');?>
             </a>
         <?php }?>
 
