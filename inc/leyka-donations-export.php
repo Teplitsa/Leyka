0bba4e0a8d0b170fb992fa51861f91f16f9f7283
Ahaenor <ahaenor@gmail.com>
2021-02-16 04:19:26 +0300

    * Small changes in donations export columns & content.

--- inc/leyka-donations-export.php
+++ inc/leyka-donations-export.php
@@ -99,7 +99,7 @@ function leyka_do_donations_export() {
         'UTF-8',
         apply_filters('leyka_donations_export_content_charset', 'windows-1251'),
         "sep=;\n".implode(';', apply_filters('leyka_donations_export_headers', array(
-            'ID', 'Имя донора', 'Email', 'Тип платежа', 'Плат. оператор', 'Способ платежа', 'Полная сумма', 'Итоговая сумма', 'Валюта', 'Дата пожертвования', 'Статус', 'Кампания', 'Подписка на рассылку', 'Email подписки', 'Комментарий'
+            'ID', 'Имя донора', 'Email', 'Тип платежа', 'Плат. оператор', 'Способ платежа', 'Полная сумма', 'Итоговая сумма', 'Валюта', 'Дата пожертвования', 'Статус', 'Кампания', 'Назначение', 'Подписка на рассылку', 'Email подписки', 'Комментарий'
         )))
     );
 
@@ -127,14 +127,16 @@ function leyka_do_donations_export() {
                     $donation->gateway_label,
                     $donation->payment_method_label,
                     $donation->amount,
-                    $donation->amount_total,
+                    str_replace('.', ',', $donation->amount_total),
                     $donation->currency_label,
-                    $donation->date,
+                    date('d.m.Y H:i:s', $donation->date_timestamp),
                     $donation->status_label,
                     $campaign->title,
+                    $campaign->payment_title,
                     $donor_subscription,
                     $donation->donor_subscription_email,
                     $donation->donor_comment,
+//                    (get_post_meta($donation->campaign_id, 'campaign_format', true)),
                 ),
                 $donation
             ))
