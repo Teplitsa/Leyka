d75b642db65d67133c3b6b1178eaddbd3d8ef235
Vyacheslav <oleinik.v89@gmail.com>
2021-10-06 13:44:07 +0600

    donations list posts_per_page fix

--- inc/donations/leyka-class-donations.php
+++ inc/donations/leyka-class-donations.php
@@ -564,6 +564,11 @@ class Leyka_Donations_Posts extends Leyka_Donations {
             default:
         }
 
+        // TODO: При очень большом кол-ве записей о пожертвованиях WP_Query отдает ошибку если
+        //  $query_params['posts_per_page'] === -1. Разобраться ограничение это WP, баг или бутылочное горлышко
+        //  производительности серверов.
+        $query_params['posts_per_page'] = $query_params['posts_per_page'] === -1 ? 99999 : $query_params['posts_per_page'];
+
         return new WP_Query($query_params);
 
     }
