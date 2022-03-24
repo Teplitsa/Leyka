d57389e8d03fa450de5b0842af3dbf5ba14f98e2
CraftyDigit <oleinik.v89@gmail.com>
2022-03-24 17:29:51 +0600

    class punycode curly braces fix

--- lib/class-punycode.php
+++ lib/class-punycode.php
@@ -412,12 +412,12 @@ class Punycode {
 
     public static function uniord($c) { // Cousin of ord() but for unicode
 
-        $ord0 = ord($c{0}); if($ord0 >=0 && $ord0 <= 127) return $ord0;
-        $ord1 = ord($c{1}); if($ord0 >= 192 && $ord0 <= 223) return ($ord0 - 192)*64 + ($ord1 - 128);
+        $ord0 = ord($c[0]); if($ord0 >=0 && $ord0 <= 127) return $ord0;
+        $ord1 = ord($c[1]); if($ord0 >= 192 && $ord0 <= 223) return ($ord0 - 192)*64 + ($ord1 - 128);
 
         if($ord0 == 0xed && ($ord1 & 0xa0) == 0xa0) return false; // Code points, 0xd800 to 0xdfff
-        $ord2 = ord($c{2}); if($ord0 >= 224 && $ord0 <= 239) return ($ord0 - 224)*4096 + ($ord1 - 128)*64 + ($ord2 - 128);
-        $ord3 = ord($c{3}); if($ord0 >= 240 && $ord0 <= 247) return ($ord0 - 240)*262144 + ($ord1 - 128)*4096 + ($ord2 - 128)*64 + ($ord3-128);
+        $ord2 = ord($c[2]); if($ord0 >= 224 && $ord0 <= 239) return ($ord0 - 224)*4096 + ($ord1 - 128)*64 + ($ord2 - 128);
+        $ord3 = ord($c[3]); if($ord0 >= 240 && $ord0 <= 247) return ($ord0 - 240)*262144 + ($ord1 - 128)*4096 + ($ord2 - 128)*64 + ($ord3-128);
 
         return false;
 
@@ -439,11 +439,11 @@ class Punycode {
 
         for($i = 0, $ix = strlen($string); $i < $ix; $i++) {
 
-            $c = ord($string{$i});
+            $c = ord($string[$i]);
 
             if($c == 0x09 || $c == 0x0a || $c == 0x0d || (0x20 <= $c && $c < 0x7e) ) $n = 0; # 0bbbbbbb
             else if(($c & 0xE0) == 0xC0) $n = 1; # 110bbbbb
-            else if($c == 0xed && (ord($string{$i + 1}) & 0xa0) == 0xa0) return false; //code points, 0xd800 to 0xdfff
+            else if($c == 0xed && (ord($string[$i + 1]) & 0xa0) == 0xa0) return false; //code points, 0xd800 to 0xdfff
             else if(($c & 0xF0) == 0xE0) $n = 2; # 1110bbbb
             else if(($c & 0xF8) == 0xF0) $n = 3; # 11110bbb
             //else if (($c & 0xFC) == 0xF8) $n=4; # 111110bb //byte 5, unnecessary in 4 byte UTF-8
@@ -451,7 +451,7 @@ class Punycode {
             else return false;
 
             for($j = 0; $j < $n; $j++) { // n bytes matching 10bbbbbb follow ?
-                if((++$i == $ix) || ((ord($string{$i}) & 0xC0) != 0x80)) {
+                if((++$i == $ix) || ((ord($string[$i]) & 0xC0) != 0x80)) {
                     return false;
                 }
             }
