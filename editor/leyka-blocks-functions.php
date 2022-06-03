31acaa778a068f98b229c2d9e65997e3a3fca83c
BootWP <burdianov@gmail.com>
2022-06-03 12:14:17 +0300

    Load scripts if in content exists at least one leyka/block

--- editor/leyka-blocks-functions.php
+++ editor/leyka-blocks-functions.php
@@ -4,8 +4,11 @@
  */
 
 // Add scripts to a page if it's post_content has leyka/form block:
-function leyka_block_modern_template_displayed($modern_template_displayed) {
-    return is_singular() && has_block('leyka/form', get_the_ID()) ? true : $modern_template_displayed;
+function leyka_block_modern_template_displayed( $modern_template_displayed ) {
+	if ( is_singular() && ( has_block('leyka/form', get_the_ID() ) || has_block('leyka/cards', get_the_ID() ) || has_block('leyka/card', get_the_ID() ) ) ) {
+		return true;
+	}
+	return $modern_template_displayed;
 }
 add_filter('leyka_modern_template_displayed', 'leyka_block_modern_template_displayed');
 
