bd5b7cda2258ca92e2e444ebc5e4a8c0050855f0
campusboy <campusboy87@gmail.com>
2023-06-28 02:10:38 +0300

    Исправляет ошибку Call to undefined function GuzzleHttp\Promise\unwrap()

--- vendor/theiconic/php-ga-measurement-protocol/src/Network/HttpClient.php
+++ vendor/theiconic/php-ga-measurement-protocol/src/Network/HttpClient.php
@@ -5,7 +5,7 @@ namespace TheIconic\Tracking\GoogleAnalytics\Network;
 use TheIconic\Tracking\GoogleAnalytics\AnalyticsResponse;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Request;
-use GuzzleHttp\Promise;
+use GuzzleHttp\Promise\Utils;
 use GuzzleHttp\Promise\PromiseInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
@@ -48,7 +48,7 @@ class HttpClient
      */
     public function __destruct()
     {
-        Promise\unwrap(self::$promises);
+        Utils::unwrap(self::$promises);
     }
 
     /**
